30b374c849e7fd21471554ccc266cc6c6c4abcdc
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-27 12:51:56 +0300

    Create 7sb1.js

---
+++ 7sb1.js
@@ -0,0 +1,15 @@
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تصميم وبرمجه: <a href='https://jawalhost.com/'>جوال هوست</a></div>").insertAfter($(".loginstat"));  },1000);
+$(`<img src="https://7sb1.com/sico/z1dq1odlt110.gif
+" class="fr" style="width:100%;margin-top: 0px;">
+
+<a id="des3" href="#" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-facebook btn btn-primary"> فيس بوك</a>
+<a id="des3" href="" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default"> اشتراكات</a>
+<a id="des3" href="#" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary"> قوانين موقع</a>
+<a id="des3" href="#" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini  fa fa-phone btn btn-default"> اتصل بنا</a>
+<a id="des22" style="padding: 2px 0px!important;font-size: 12px !important;width:105px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 4, 161);" class="label label label-primary mini fl" target="_blank" href="https://7sb1.com"> استايلات / حسبتك لي  </a>
+`).insertBefore('.nav-tabs');
+
+$(`<link rel="stylesheet" type="text/css" href=""><style>.des {position: absolute;transform:matrix(1, 0, 0, 1, 86, 0);margin-top: 2px;width: 300px;height: 60px;float: left;background: transparent url(https://7sb1.com/sico/z1dq1sefak10.png) no-repeat scroll right top;text-align: left;}button.btn.fr.btn-success.fa.fa-refresh {display: none;}a.label.label-primary.fl img.fl {display: none;}a.label.label-primary.fl {border-radius: 3px!important;margin: 2px -156px;padding: 5px!important;margin-bottom: 2px;text-align: right!important;}#des22 {text-align: center!important;font-size: 100%!important;}label.fl.label.loginstat {background-color: transparent!important;position: absolute;top: 0;margin-top: 4px;left: 0;}.fr.borderg, .fr.borderg a {font-size: 10px !important;padding: 0 5px!important;}span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {padding: 0px 3px 2px;margin-right: 5px;}label.label.label-primary.mini.fl {width: 64%!important;padding: 1px;border-radius: 3px!important;}</style>`).insertBefore('body');
+$.getScript("https://rawcdn.githack.com/waleed-qaid/des/fb5ecfb0a41265477c14936b5f3e7d2d8ab2598b/www.1des.net- te3b-color.js");$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">`)
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;اريحو : مسإمعكم ..</a><input type="jfa-w" name="FirstName" value="  شـإت حسبتك لي "><audio src="https://j.top4top.io/m_1592gehs81.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
